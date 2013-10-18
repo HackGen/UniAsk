fc7f0d64601d22433ad83a10ffa1fd3b270e5342
rampant <rampant1018@gmail.com>
2013-10-18 23:20:42 +0800

    Modify stylesheet path

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -3,7 +3,6 @@
 <head>
 	<meta http-equiv="Content-type" content="text/html; charset=utf8" />
 	<title>UniAsk :: 由你問大學</title>
-	<base href="<?php echo base_url();?>" />
 	<link rel="stylesheet" type="text/css" href="css/style.css" media="screen" />
 	<script src="http://code.jquery.com/jquery-latest.min.js"></script>
 </head>
