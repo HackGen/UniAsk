5fda9838aac3d893d0e9385ab68f716b0240e872
iGaryGu <gary640201a@gmail.com>
2013-10-19 16:01:10 +0800

    question load answer view

--- application/views/answer/view_text.php
+++ application/views/answer/view_text.php
@@ -29,13 +29,13 @@
 <div id="myModal" class="modal hide fade" tableindex="-1" role="dialog" aria-labelledby="myModalLabel" aria-hidden="true">
 	<div class="modal-header">
 		<button type="button" class="close" data-dismiss="modal"aria-hidden="true">x</button>
-		      <h3 id="myModalLabel">感謝你的回答</h3>
+		      <h3 id="myModalLabel">是否送出？</h3>
 	</div>
 	<div class="modal-body">
 		<p>Finish！</p>
 	</div>
 	<div class="modal-footer">
-		 <button class="btn" data-diamiss="modal" aria-hidden="true">關閉</button>
+		 <button class="btn" data-diamiss="modal" aria-hidden="true">確定</button>
 	</div>
 </div>
 
