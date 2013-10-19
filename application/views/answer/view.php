d96e391913ea128829e086ef3058895717c1bec5
iGaryGu <gary640201a@gmail.com>
2013-10-19 10:47:21 +0800

    new answer view

---
+++ application/views/answer/view.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html>
+
+<?php
+echo $user_id."\n";
+echo $date."\n";
+echo $content."\n";
+echo $fb_id."\n";
+echo $email."\n";
+
+?>
+<div class="view_pic"> 
+<img src="https://graph.facebook.com/'<?php $fb_id ?> '/picture?height=100&width=100" />
+</div>
+
+</html>
