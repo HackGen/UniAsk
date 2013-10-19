aa698bafa55b5a541f25441e9d4cd7842a8e4f0f
iGaryGu <gary640201a@gmail.com>
2013-10-19 10:13:54 +0800

    rewrite answer.php

--- application/controllers/answer.php
+++ application/controllers/answer.php
@@ -12,7 +12,17 @@ class Answer extends CI_Controller
 	}
 	public function view($ques_id)
 	{
-		echo $ques_id ;
+		//echo $ques_id ;
+		$query = $this->db->query("SELECT * FROM `questions` WHERE `question_id`='".$ques_id."'");
+		if($query->num_rows() > 0){
+			foreach($query->result() as $questions){
+				echo "<img src=".$this->session->userdata('img')." height='35px' title=".$this->session->userdata('name')." />";
+				echo $questions['date'];
+				echo $questions['content'];			
+			}
+		}
+		
+		
 	}
 	
 }
