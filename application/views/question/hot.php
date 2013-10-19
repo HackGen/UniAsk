02994c80b3689981995901821e8196e06af02296
iGaryGu <gary640201a@gmail.com>
2013-10-20 00:13:01 +0800

    index

--- application/views/question/hot.php
+++ application/views/question/hot.php
@@ -51,8 +51,8 @@
 	
 		<div id="view_div">
 			<div class="view_post">
-				<span class="tag1"><a href="search/get/<?php echo $questions['catalog_school'] ?"><?php echo $questions['catalog_school'] ?></a></span>
-				<span class="tag2"><a href="search/get/<?php echo $questions['catalog_detail'] ?"><?php echo $questions['catalog_detail'] ?></a></span>
+				<span class="tag1"><a href='search/get/<?php echo $questions['catalog_school']; ?>'><?php echo $questions['catalog_school']; ?></a></span>
+				<span class="tag2"><a href='search/get/<?php echo $questions['catalog_detail']; ?>'><?php echo $questions['catalog_detail']; ?></a></span>
 				<a href = 'http://114.35.129.223/UniAsk/question/view/<?php echo $questions['question_id'] ; ?>' ><?php echo $questions['content'] ?></a><br/>
 				by <strong><?php echo $user['name'];?></strong><span class="date"><?php echo date("M d Y",$questions['date']);?></span>
 			</div>
