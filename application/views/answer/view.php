9cb3210fcfb8d1dabb3fcb76dde1ba5fa38c5fa8
iGaryGu <gary640201a@gmail.com>
2013-10-19 16:45:28 +0800

    toolbar init

--- application/views/answer/view.php
+++ application/views/answer/view.php
@@ -1,8 +1,12 @@
-<?php
-echo $name ." : <br>";
-echo $content ."<br>";
-echo "date : ".$date."<br><br><br>";
-
-
-
-?>
+<div id="div">
+	<div id="view_div">
+		<div class="view_pic">
+			<img src="https://graph.facebook.com/<?php echo $user['fb_id'];?>/picture?height=100&width=100" />
+		</div>
+		<div class="view_post">
+			<strong><?php echo $name;?> 回答:</strong><br/>
+			<?php echo $content; ?>
+			<span class="date"><?php echo date("M d Y",$date);?></span>
+		</div>
+	</div>
+</div>
