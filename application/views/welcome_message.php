777f583fa7d0b0779b7931aa90cd41d706236593
rampant <rampant1018@gmail.com>
2013-10-19 13:32:03 +0800

    test database

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -43,6 +43,7 @@
 		echo "<div id='question_create_button' style='margin:20px;'>最新問題</div>";
 		$data['question'] = $this->question_model->get_question();
 		$data['hot'] = $this->question_model->get_hot_question();
+		echo $data['hot'];
 		//$this->load->view('question/all', $data);
 		$this->load->view('question/hot', $data);
 		$this->load->view('question/latest', $data);
