9c08f7b0f1361b5c2a5be00836555c3c2fee39ea
iGaryGu <gary640201a@gmail.com>
2013-10-19 15:33:38 +0800

    question load answer view

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -10,7 +10,6 @@
     	<link rel="stylesheet" href="css/stylesheet.css">
 	<script src="http://code.jquery.com/jquery.js"></script>
 	<script src="<?php echo base_url(); ?>/assets/bootstrap/js/bootstrap.min.js"></script>
-	<meta name="viewport" content="width=device-width,initial-scale="1.0"">
 	<link href="<?php echo base_url(); ?>/assets/bootstrap/css/bootstrap.min.css" rel="stylesheet" media="screen">
 	
 </head>
