75ee8beec1529b5152a562af952c503c07f8bd20
iGaryGu <gary640201a@gmail.com>
2013-10-19 23:53:08 +0800

    index

--- application/views/search/index.php
+++ application/views/search/index.php
@@ -13,14 +13,17 @@
 #search {
 	display: inline-block;
 	text-align: center;
-	margin-top: 140px;
+	margin-top: 120px;
 	color: #fff;
 }
 
+#search h1 {
+	font-size: 30px;
+}
 
 #search input[type="text"] {
     background: url("http://www.cjies.com/etop/images/search-white.png") no-repeat 12px 15px #fff;
-    border: 1px solid #d1d1d1;
+    border: 3px solid #d1d1d1;
     color: #333;
 	font-size: 20px;
 	text-align: left;
@@ -48,7 +51,7 @@
 
 <div id="div">
 	<div id="search">
-		<h1>UniAsk</h1>
+		<h1>UNIASK</h1>
 		<input type="text" id="search_text2" placeholder="搜尋你覺得困擾的問題..." />
 		<br/><br/>
 		例子: 交通大學 假單 | 如何繳費? | 獎學金
