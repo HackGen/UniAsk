bf9f412dcb4c2ddbe507f4d4cea67e39a3c78d8c
iGaryGu <gary640201a@gmail.com>
2013-10-20 00:50:04 +0800

    footer

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,5 +1,6 @@
 <footer>
-	2013 Uniask
+	All rights reserved. copyright © 2013 by RTCG, Taiwan R.O.C.<br/>
+	<a href="http://github.com/HackGen/UniAsk" target="_blank">Contact Us</a>
 </footer>
 </body>
 </html>
