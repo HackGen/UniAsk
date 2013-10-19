8dce7571ae0e08d5ce542f753c8c1f3526127e47
cjies <cijies@gmail.com>
2013-10-19 15:18:04 +0800

    change index

--- application/views/welcome_message.php
+++ application/views/welcome_message.php
@@ -45,24 +45,24 @@
 		//$this->load->view('question/all', $data);
 	?>
 
-	
-	<div id="hot_question_field">
-		<div class='container_title' style='margin:20px;'>熱門問題</div>
-		<?php
-			$data['question'] = $this->question_model->get_hot_question();
-			$this->load->view('question/hot', $data);
-		?>
-	</div>
+	<div id="div">
+		<div id="hot_question_field">
+			<div class='container_title' style='margin:20px;'>熱門問題</div>
+			<?php
+				$data['question'] = $this->question_model->get_hot_question();
+				$this->load->view('question/hot', $data);
+			?>
+		</div>
 
 
-	<div id="latest_question_field">
-		<div class='container_title' style='margin:20px;'>最新問題</div>
-		<?php
-			$data['question'] = $this->question_model->get_question();
-			$this->load->view('question/hot', $data);
-		?>
+		<div id="latest_question_field">
+			<div class='container_title' style='margin:20px;'>最新問題</div>
+			<?php
+				$data['question'] = $this->question_model->get_question();
+				$this->load->view('question/hot', $data);
+			?>
+		</div>
 	</div>
-	
 	
 	
 </div>
