f62ea34530899c0c8f33a0c10017306a26cf6d59
iGaryGu <gary640201a@gmail.com>
2013-10-19 11:13:25 +0800

    toolbar init

--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -4,7 +4,7 @@
 	<title><?php echo $title; ?></title>
 	<link rel="stylesheet" type="text/css" href="<?php echo base_url(); ?>/assets/style.css" media="screen" />
 	<script src="http://code.jquery.com/jquery-latest.min.js"></script>
-	<script src="/wysinhtml5/parser_rules/advanced.js"></script>
+	<script src="/wysihtml5/parser_rules/advanced.js"></script>
 	<script src="/wysihtml5/dist/wysihtml5-0.3.0.min.js"></script>
 	<script>
 		var editor = new wysihtml5.Editor("wysihtml5-textarea", { // id of textarea element
