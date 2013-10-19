65ff4954f1560b90b457ff24e70bf27cb350c3c3
cjies <cijies@gmail.com>
2013-10-19 13:03:32 +0800

    search condition

--- application/models/search_model.php
+++ application/models/search_model.php
@@ -6,15 +6,47 @@ class Search_model extends CI_Model {
 		$this->load->database();
 	}
 	
+	/*
 	public function get_search($search_keyword = FALSE)
 	{
 		$search_keywords = explode(' ', trim($search_keyword));
 		
 		foreach($search_keywords as $search_keyword) {
+			$this->db->or_like("", $search_keyword);
 			$this->db->or_like("content", $search_keyword);
 		}
 		$query = $this->db->get("questions");
 		return $query->result_array();
+	}
+	*/
+	
+	public function get_search($search_keyword = FALSE)
+	{
+		//$search_keywords = explode(' ', trim($search_keyword));
+		
+		$parts = substr_count(trim($search_keyword), ' ');
+
+		switch($parts){
+			case 0:
+			$this->db->or_like('catalog_school', $search_keyword);
+			$this->db->or_like('catalog_school', $search_keyword);
+			$this->db->or_like('content', $search_keyword);
+			break;
+		case 1;
+			$this->db->or_like('CONCAT(catalog_school, " ", content)', $search_keyword);
+			$this->db->or_like('CONCAT(catalog_detail, " ", content)', $search_keyword);
+			$this->db->or_like('CONCAT(catalog_school, " ", catalog_detail)', $search_keyword);
+			break;
+		case 2:
+		default:
+			$this->db->or_like('CONCAT(catalog_school, " ", catalog_detail, " ", content)', $search_keyword);
+			break;
+
+		}	
+
+		$query = $this->db->get('questions');
+		return $query->result_array();
+		
 	}
 	
 	
