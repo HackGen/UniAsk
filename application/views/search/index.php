7e1b1379ed980a31faf43a9769a67bc13c9565fb
cjies <cijies@gmail.com>
2013-10-19 14:32:48 +0800

    change search/index.php

--- application/views/search/index.php
+++ application/views/search/index.php
@@ -10,35 +10,31 @@
 	
 }
 
-#search2 {
+#search {
 	display: inline-block;
-	text-align: center;
-	margin-top: 160px;
-	
+	text-align: left;
+	padding: 20px;
+	color: #fff;
 }
 
 
-#search2 input[type="text"] {
-    background: url("http://www.cjies.com/etop/images/search-white.png") no-repeat 12px 15px #fff;
+#search input[type="text"] {
+    background: url("http://www.cjies.com/etop/images/search-white.png") no-repeat 10px 10px #fff;
     border: 1px solid #d1d1d1;
     color: #333;
-	font-size: 20px;
-	text-align: left;
-    width: 600px;
-    padding: 9px 15px 10px 35px;
+	font-size: 16px;
+    width: 400px;
+    padding: 8px 15px 8px 35px;
     -webkit-border-radius: 5px;
     -moz-border-radius: 5px;
     border-radius: 5px;
-	-moz-box-shadow: 0 0 8px #999;
-	-webkit-box-shadow: 0 0 8px#999;
-	box-shadow: 0 0 8px #999;
     -webkit-transition: all 0.20s ease-in-out;
   	-moz-transition: all 0.20s ease-in-out;
   	-ms-transition: all 0.20s ease-in-out;
   	-o-transition: all 0.20s ease-in-out;
 }
 
-#search2 input[type="text"]:focus {
+#search input[type="text"]:focus {
     color: #333;
     outline: none;
     border-color: #aaa;
@@ -47,7 +43,7 @@
 </style>
 
 <div id="div">
-	<div id="search2">
+	<div id="search">
 		<input type="text" id="search_text" placeholder="搜尋你覺得困擾的問題..." />
 		<br/><br/>
 		例子: 交通大學 假單 | 如何繳費? | 獎學金
