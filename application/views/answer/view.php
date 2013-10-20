55a2fe84d744982ef3e5a3bd21676c33e0af574b
iGaryGu <gary640201a@gmail.com>
2013-10-20 10:05:38 +0800

    view.php

--- application/views/answer/view.php
+++ application/views/answer/view.php
@@ -7,6 +7,8 @@
 		<div class="view_post">
 			<strong><?php echo $name;?> 回答:</strong><br/>
 			<?php echo $content; ?>
+			<input type="submit" name="submit" value="+" />
+			<input type="submit" name="submit" value="-" />
 			<span><?php echo date("M d Y",$date);?></span>
 		</div>
 	</div>
