92da13462763dc6ce81b12cedae479963184760e
cjies <cijies@gmail.com>
2013-10-20 10:05:02 +0800

    change search results ui

--- application/views/question/all.php
+++ application/views/question/all.php
@@ -79,7 +79,7 @@
 				<span class="tag1"><a href='search/get/<?php echo $questions['catalog_school']; ?>'><?php echo $questions['catalog_school']; ?></a></span>
 				<span class="tag2"><a href='search/get/<?php echo $questions['catalog_detail']; ?>'><?php echo $questions['catalog_detail']; ?></a></span><br/>
 				<a class="view_post_link" href = 'http://114.35.129.223/UniAsk/question/view/<?php echo $questions['question_id'] ; ?>' ><?php echo $questions['content'] ?></a><br/>
-				<span class="user">by <strong><?php echo $user['name'];?></strong>&nbsp;|&nbsp;<?php echo date("M d Y",$questions['date']);?><span>
+				<div class="user">by <strong><?php echo $user['name'];?></strong>&nbsp;|&nbsp;<?php echo date("M d Y",$questions['date']);?></div>
 				<span class="date"><strong><?php echo $count_answer;?></strong> 回答</span>
 			</div>
 		</div>
