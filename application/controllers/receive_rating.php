fa3d14928df6626c44aeba15a552007ea6d03968
iGaryGu <gary640201a@gmail.com>
2013-10-20 11:24:19 +0800

    t

--- application/controllers/receive_rating.php
+++ application/controllers/receive_rating.php
@@ -10,9 +10,10 @@ class Receive_rating extends CI_Controller{
 
 	public function plus($ans_id){
 		echo $ans_id;
-		$query = $this->db->query("SELECT * FROM `answer` WHERE `answer_id`='".$ans_id."'");
+		$query = $this->db->query("SELECT * FROM `answer` WHERE answer_id='".$ans_id."'");
 		foreach($query->result() as $plus){
 			$rating_plus = $plus->rating_plus;
+			print_r($plus);
 		}
 		$rating_plus = $rating_plus+1;
 		echo $rating_plus;
